caeb057e7c8ac23c1aeaab464043edf8a7c6c21f
Melanie Appleby <applebym12@gmail.com>
2016-02-11 11:49:20 -0500

    Q5

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,19 @@
-PLACE YOUR CODE HERE
+import csv
+import advanced_python_regex as apr
+
+
+def write_emails(list):
+    with open('emails.csv', 'wb') as file:
+        writer = csv.writer(file)
+        for email in list:
+            writer.writerow([email])
+
+def main():
+    data = apr.read_data('faculty.csv')
+    email_list = apr.get_emails(data)
+    write_emails(email_list)
+
+if __name__ == '__main__':
+    main()
+
+
